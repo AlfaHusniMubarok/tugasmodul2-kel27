af87bd9b73a02bbe09052013fc14e54d56921538
Alfa Husni Mubarok <alfahusnimubarok@gmail.com>
2021-09-11 14:37:41 +0700

    Commit Pertama - Alfa

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div className="C">
+      <div
+        className="flex items-center justify-center bg-gray-50"
+        style={{ height: "100vh" }}
+      >
+            <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl text-center">
+        <span className="block">Kelompok 27 Praktikum RPLBK</span>
+      <span className="block text-indigo-600">Tugas Modul 2</span>
+      <p className="text-center text-blue-200 font-normal">
+        Ini tugas atau kasih Ibu? <br/>
+        Kok tak terhingga sepanjang masa :) <br/>
+        #banyaktugas
+      </p>
+    </h2>
+  </div>
+</div>
+);
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,16 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import FeatureSections from "./components/FeatureSections";
+import Quotes from "./components/Quotes";
+import Avatar from "./components/Avatar";
+import Gambar from "./assets/images/image.jpeg";
+const quotes = "Jangan pernah menyerah sebelum kesempatan itu benar-benar sudah hilang";
 
+const data = [{
+  name: "Alfa Husni Mubarok", nim: "21120118130064", kelompok: "27", email: "alfahusnimubarok@gmail.com", ttl: "Demak, 07 September 1998",
+},
+{name: "Ahmad Hufron", nim: "211201181200__", kelompok: "27", email: "________@gmail.com", ttl: "___, ____________",
+}];
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Avatar image = {Gambar} avatarname="Alfa Husni Mubarok" avatarnim="21120118130064" />
+    <FeatureSections data={data} isNameBold catatan="Kerja Bagus Kawan" />
+    <Quotes author="~~Al Hm~~" quotes={quotes} />
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
